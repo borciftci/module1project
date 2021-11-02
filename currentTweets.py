ef458d80a17d68f22abb9e66cdf85d7899c5946d
SimonHutten <s.hutten@student.utwente.nl>
2021-11-02 14:40:29 +0100

    time factor --> 1

--- currentTweets.py
+++ currentTweets.py
@@ -6,7 +6,7 @@ root_content_path = 'currentTweets_static'
 
 @event('init')
 def setup(ctx, e):
-    start_offline_tweets('data/sports_tweets.txt', event_name='chrip', time_factor = 1000)
+    start_offline_tweets('data/sports_tweets.txt', event_name='chrip', time_factor = 1)
 
 @event('chrip')
 def tweet(ctx, e):
