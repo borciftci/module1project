5e1a912ee46aa36743c3ee917275978d64728412
borciftci <90639949+borciftci@users.noreply.github.com>
2021-11-02 12:07:17 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,19 @@
+from eca import * 
+
+from eca.generators import start_offline_tweets
+import datetime
+import textwrap
+
+@event('init')
+def setup(ctx, e):
+    start_offline_tweets('data/sports_tweets.txt', event_name='chirp', time_factor = 1000000000)
+
+@event('chirp')
+def tweet(ctx, e):
+    # we receive a tweet
+    tweet = e.data
+    # nicify text
+    text = tweet['text']
+
+    # generate output
+    emit('tweet', text)
